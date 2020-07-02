fa211aa89d746f99921c79cef700b40b7aa75062
Evan Sá <54774654+EvanDias@users.noreply.github.com>
2020-07-02 21:12:03 +0100

    Delete board.h

--- board.h
+++
@@ -1,9 +0,0 @@
-#include "initGame.h"
-
-void printBoard(void *gameStruct, int sizeBoard);
-void printBothBoard(User *start, User *other, int sizeBoard);
-void letters(int size);
-int charToIntLower(char chInput);
-int charToIntUpper(char chInput);
-void printSinkShip(void *structure, int x, int y);
-int choiceChar(char c) ;
