416db44a93a2ad84dc9852aa2078f70ea4f08579
Evan Sá <54774654+EvanDias@users.noreply.github.com>
2020-07-02 21:09:57 +0100

    Delete randomShips.h

--- randomShips.h
+++
@@ -1,9 +0,0 @@
-#ifndef RANDOM_RANDOMsHIPS_H
-#define RANDOM_RANDOMsHIPS_H
-#include "game.h"
-
-void randomTranslation(ListNode *node);
-void randomRotation(ListNode *node);
-void randomInsert(User *usr, ListNode *node, int boardSize);
-
-#endif //RANDOM_RANDOMsHIPS_H
