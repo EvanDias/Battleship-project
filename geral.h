02d50cf5239b1219ec7df816d658d211c08ec17f
Evan Sá <54774654+EvanDias@users.noreply.github.com>
2020-07-02 21:11:27 +0100

    Delete geral.h

--- geral.h
+++
@@ -1,8 +0,0 @@
-#include "cell.h"
-
-
- void *initDataStructure(int boardSize); 
- void *searchPoint(void *structure, Point *p);
- void insertShip(void *structure,  void *information, Point *p);
- void deleteShip(void *structure, void *data, Point *p);
- void freeDataStructure(void *structure);
